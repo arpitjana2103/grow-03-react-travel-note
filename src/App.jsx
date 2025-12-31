4a33322c4ee8236e040ade58ada1ee043e5747f3
Arpit Jana <arpitjana2103@gmail.com>
2025-12-31 08:41:39 +0530

    feature : page setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import ErrorPage from "./pages/Errorpage";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
     return (
         <RouterProvider
             router={createBrowserRouter([
+                { path: "/", element: <HomePage /> },
+                { path: "/product", element: <ProductPage /> },
                 { path: "*", element: <ErrorPage /> },
             ])}
         />
